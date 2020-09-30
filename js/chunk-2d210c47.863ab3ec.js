6a458de17627ccd2acd5a6ab0d34ed611f31da85
Mihai <16misha9.4@gmail.com>
2020-09-30 11:07:58 +0300

    deploy

---
+++ js/chunk-2d210c47.863ab3ec.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d210c47"],{b8fa:function(t,n,e){"use strict";e.r(n);var a=function(){var t=this,n=t.$createElement;t._self._c;return t._m(0)},c=[function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"about"},[e("h1",[t._v("This is an contact page")])])}],s=e("2877"),u={},i=Object(s["a"])(u,a,c,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d210c47.863ab3ec.js.map
